1435664238281054fc67fab3756b7b8c9f3b4b46
DP <dineshparadesi94@gmail.com>
2020-05-26 01:57:55 +1000

    Username Validation

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
-export class SignupFormComponent implements OnInit {
+export class SignupFormComponent {
+reactiveform = new FormGroup({
+  username: new FormControl('',[
+    Validators.required,
+    Validators.minLength(3)
+  ]),
+  password: new FormControl('',Validators.required)
+});
 
+get username(){
+  return this.reactiveform.get('username');
+}
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
