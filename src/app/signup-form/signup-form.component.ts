53eb339763e6bc7e1eb7f899aa25e21e042a55c9
DP <dineshparadesi94@gmail.com>
2020-05-26 16:32:32 +1000

    Nested Group - Account

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -8,17 +8,27 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignupFormComponent {
 reactiveform = new FormGroup({
-  username: new FormControl('',[
-    Validators.required,
-    Validators.minLength(3),
-    UsernameValidators.cannotContainSpace
-  ],
-  UsernameValidators.shouldBeUnique),
-  password: new FormControl('',Validators.required)
+  account: new FormGroup({
+    username: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace
+    ],
+    UsernameValidators.shouldBeUnique),
+    password: new FormControl('',Validators.required)
+  })
 });
 
 get username(){
-  return this.reactiveform.get('username');
+  return this.reactiveform.get('account.username');
+}
+login(){
+  this.reactiveform.setErrors({
+    invalidLogin: {
+      username:'error',
+      password:'error'}
+  });
+
 }
   constructor() { }
 
