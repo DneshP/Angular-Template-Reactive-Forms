62fb91de1d5635dba3a1412be6e7a6f61130144b
DP <dineshparadesi94@gmail.com>
2020-05-29 14:32:28 +1000

    Add topics

--- src/app/course-list-form/course-list-form.component.ts
+++ src/app/course-list-form/course-list-form.component.ts
@@ -12,8 +12,17 @@ export class CourseListFormComponent implements OnInit {
   });
 
   addTopic(topic: HTMLInputElement){
-    (this.form.get('topics') as FormArray).push(new FormControl(topic.value));
+  this.topics.push(new FormControl(topic.value));
+  topic.value='';
   }
+  removeTopic(topic: FormControl){
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+  get topics(){
+    return this.form.get('topics') as FormArray;
+  } 
+  
   constructor() { }
 
   ngOnInit() {
